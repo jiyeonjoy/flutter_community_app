e17fc40d3368cbd0069811ad626eb23a919aa95b
최지연 <choi02647@naver.com>
2023-10-27 13:59:03 +0900

    prettyDioLogger 추가

--- lib/data/data_sources/remote/http_client.dart
+++ lib/data/data_sources/remote/http_client.dart
@@ -1,5 +1,17 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_community_app/app/common/constants.dart';
+import 'package:flutter_community_app/data/data_sources/remote/apis/users_api.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+final prettyDioLogger = PrettyDioLogger(
+  requestHeader: true,
+  requestBody: true,
+  responseBody: true,
+  responseHeader: false,
+  error: true,
+  compact: true,
+  maxWidth: 90,
+);
 
 class CommunityHttpClient {
   static final CommunityHttpClient _instance = CommunityHttpClient._internal();
@@ -17,8 +29,15 @@ class CommunityHttpClient {
 
   Dio? _dio;
   Dio get dio {
-    _dio ??= Dio(options);
+    _dio ??= Dio(options)..interceptors.addAll([
+      prettyDioLogger,
+    ]);
 
     return _dio!;
   }
+
+  UsersApi get usersApi => UsersApi(
+      dio,
+      baseUrl: AppConstants.DOMAIN_URL,
+  );
 }
