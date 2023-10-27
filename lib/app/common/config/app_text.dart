0eef8d3f2b58f79fd5e34b1a6a2279e9bd25b7bd
최지연 <choi02647@naver.com>
2023-10-27 22:57:28 +0900

    로그인 페이지 ui

--- lib/app/common/config/app_text.dart
+++ lib/app/common/config/app_text.dart
@@ -12,4 +12,5 @@ class AppTexts {
   final String myList = '내 리스트';
   final String login = '로그인';
   final String loginPage = '로그인 페이지';
+  final String loginTextFieldHint = '이메일을 입력해 주세요.';
 }
--- lib/app/login/ui/login_page.dart
+++ lib/app/login/ui/login_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_community_app/app/common/config/r.dart';
 import 'package:flutter_community_app/app/common/ui/app_bar_view.dart';
 import 'package:flutter_community_app/app/common/ui/common_button.dart';
-import 'package:flutter_community_app/app/common/ui/custom_text_field.dart';
+import 'package:flutter_community_app/app/common/ui/common_text_field.dart';
 import 'package:flutter_community_app/app/login/controller/login_page_controller.dart';
 import 'package:get/get.dart';
 
@@ -17,17 +17,21 @@ class LoginPage extends GetView<LoginPageController> {
         title: R.string.loginPage,
         leading: const SizedBox(),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const Spacer(),
-            const CustomTextField(),
-            const Spacer(),
-            CommonButton(R.string.login),
-            const Spacer(),
-          ],
-        ),
-      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Spacer(),
+          CommonTextField(
+            hintText: R.string.loginTextFieldHint,
+            keyboardType: TextInputType.emailAddress,
+          ),
+          const Spacer(),
+          CommonButton(
+            R.string.login,
+          ),
+          const SizedBox(height: 20),
+        ],
+      ).marginSymmetric(horizontal: 24),
     );
   }
 }
