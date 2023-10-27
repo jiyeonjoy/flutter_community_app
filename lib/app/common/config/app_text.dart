c5b8776a91951009816a097fb1395796ef0a8dc1
최지연 <choi02647@naver.com>
2023-10-27 11:30:14 +0900

    root page tab view 추가

--- lib/app/common/config/app_text.dart
+++ lib/app/common/config/app_text.dart
@@ -6,5 +6,8 @@ class AppTexts {
   AppTexts._internal();
 
   final String buttonYes = '예';
-  final String buttonNo= '아니요';
+  final String buttonNo = '아니요';
+
+  final String list = '리스트';
+  final String myList = '내 리스트';
 }
--- lib/app/root/controller/root_page_controller.dart
+++ lib/app/root/controller/root_page_controller.dart
@@ -1,6 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_community_app/app/common/config/r.dart';
 import 'package:get/get.dart';
 
 class RootPageController extends GetxController {
   static RootPageController get to => Get.find();
 
+  RootTab currentTab = RootTab.list;
+
+  void changeTab(int index) {
+    currentTab = RootTab.from(index);
+    update();
+  }
+}
+
+enum RootTab {
+  list(0),
+  myList(1);
+
+  final int id;
+  const RootTab(this.id);
+
+  static RootTab from(int id) {
+    for (final value in RootTab.values) {
+      if (value.id == id) {
+        return value;
+      }
+    }
+    return RootTab.list;
+  }
+}
+
+extension RootTabExt on RootTab {
+  Widget get icon {
+    switch (this) {
+      case RootTab.list:
+        return Icon(
+          Icons.list_alt_outlined,
+          color: R.color.black.withOpacity(0.5),
+          size: 24.0,
+        );
+      case RootTab.myList:
+        return Icon(
+          Icons.person_2_outlined,
+          color: R.color.black.withOpacity(0.5),
+          size: 24.0,
+        );
+    }
+  }
+
+  Widget get selectedIcon {
+    switch (this) {
+      case RootTab.list:
+        return Icon(
+          Icons.list_alt,
+          color: R.color.black,
+          size: 24.0,
+        );
+      case RootTab.myList:
+        return Icon(
+          Icons.person_2,
+          color: R.color.black,
+          size: 24.0,
+        );
+    }
+  }
+
+  String get title {
+    switch (this) {
+      case RootTab.list:
+        return R.string.list;
+      case RootTab.myList:
+        return R.string.myList;
+    }
+  }
 }
